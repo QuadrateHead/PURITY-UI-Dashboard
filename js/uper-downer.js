462d3c2e380fb515ced950f3dacaf100e95a5411
QuadrateHead <korcinskiy.ivan@gmail.com>
2026-02-28 17:31:17 +0100

    Table Finnish ans some fixes

--- js/uper-downer.js
+++ js/uper-downer.js
@@ -1,9 +1,20 @@
-const chars = document.querySelectorAll(".upps-downs-char")
+const chars = document.querySelectorAll(".upps-downs-char");
 chars.forEach(char => {
    if (char.textContent == "+"){
-      char.closest(".upps-downs").classList.add("upps-downs--upps")
+      char.closest(".upps-downs").classList.add("upps-downs--upps");
    }
    else{
-      char.closest(".upps-downs").classList.add("upps-downs--downs")
+      char.closest(".upps-downs").classList.add("upps-downs--downs");
+   }
+});
+
+const statuses = document.querySelectorAll(".projects__t-status-a-value");
+statuses.forEach(state => {
+   if (state.textContent === "Online"){
+      state.style.backgroundColor = "#48bb78";  
+   }
+   else if(state.textContent === "Offline"){
+      state.style.color = "var(--status-text)"; 
+      state.style.backgroundColor = "var(--status-bg)"; 
    }
 });
